bfbaa600eb81898c0e9f911fb2fa91d7b643eaf6
Anna Vestman <anna.v.westman@gmail.com>
2026-01-26 21:13:18 +0100

    register and login page html and css done. Added icons and images. Started design on landing page

--- js/header.js
+++ js/header.js
@@ -3,9 +3,16 @@ class PageHeader extends HTMLElement {
   this.innerHTML = `
 
 <header class="mainHeader">
+  <div class="innerHeader">
+    <div class="logo">
+     <a href="/index.html">
+    <img src="/icons/Mobile Logo.svg"
+    class="logoMobile"
+    alt="FreeTime mobile logo"></a>
 
-  <div class="logo">
-    <img src="/icons/Mobile Logo.svg">
+  <img src="/icons/FreeTime_Feed_brown_desktop.svg" 
+    class="logoDesktop"
+    alt="FreeTime Feed desktop logo">
   </div>
 
   <nav class="navLinks">
@@ -16,43 +23,71 @@ class PageHeader extends HTMLElement {
   </nav>
 
   <div class="headerIcons">
-    <div class="searchHeader">
-  <img src="/icons/searchIcon.png" alt="Search icon"></div>
-
+      <img class="searchIconHeader"src="/icons/searchIconHeader.svg" alt="Search icon">
+      <img class="adminDeskHeader" src="/icons/adminIconHeader.png" alt="Sign in logo">
  <button id="hamburger" class="hamburgerBtn">
 
-  <img 
-    src="/icons/hamburgerIcon.png" 
-    class="iconOpen">
+  <img src="/icons/hamburgerIconGreen.svg" class="iconOpen">
+        <img src="/icons/closeIconGreen.svg" class="iconClose">
+      </button>
+    </div>
+</header>
 
-  <img 
-    src="/icons/hamburgerClose.png" 
-    class="iconClose">
 
-</button>
-
-</div>
-
-  <!-- MOBILE MENU -->
+  <!---------- MOBILE MENU ------------>
 
   <div class="mobileMenu">
     <div class="menuViewer">
-      <a href="/">Home</a>
-      <a href="/posts.html">Latest Posts</a>
-      <a href="/about.html">About</a>
-      <a href="/contact.html">Contact</a>
+
+      <a href="/" class="menuItem">
+      <img src="/icons/homeIcon.png" alt="House icon">
+      <span>Home</span>
+      </a>
+
+      <a href="#" class="menuItem">
+      <img src="/icons/chatIcon.png" alt="Chat icon">
+      <span>Latest Posts</span>
+      </a>
+
+      <a href="#" class="menuItem">
+      <img src="/icons/aboutIcon.png" alt="Info icon">
+      <span>About</span>
+      </a>
+
+      <a href="#" class="menuItem">
+      <img src="/icons/emailIcon.png" alt="Contact icon">
+      <span>Contact</span>
+      </a>
+
     </div>
 
     <div class="menuAdmin hidden">
-      <hr>
-      <a href="/admin/dashboard.html">Dashboard</a>
-      <a href="/admin/create.html">Create Post</a>
-      <a href="#" id="logoutBtn">Sign Out</a>
+     <a href="/account/login.html" class="menuItem">
+      <img src="/icons/adminIcon.png" alt="Login icon">
+      <span>Admin login</span>
+      </a>
+
+     <!-- TOGGLE OFF ATM.
+     
+     <a href="#" class="menuItem">
+      <img src="/icons/dashboardIcon.png" alt="Dashboard icon">
+      <span>Dashboard</span>
+      </a>
+
+      <a href="#" class="menuItem">
+      <img src="/icons/createPost.png" alt="Create post icon">
+      <span>Create Post</span>
+      </a>
+    
+      <a href="#" class="menuItem">
+      <img src="/icons/signOut.png" alt="Sign out icon">
+      <span>Sign out</span>
+      </a> -->
     </div>
 
   </div>
 
-</header>
+
 
     `;
 
@@ -60,6 +95,7 @@ class PageHeader extends HTMLElement {
  }
 
  init() {
+  // const DEV_SIDEBAR_OPEN = true;
   const hamburger = this.querySelector("#hamburger");
   const mobileMenu = this.querySelector(".mobileMenu");
   const adminMenu = this.querySelector(".menuAdmin");
@@ -72,10 +108,16 @@ class PageHeader extends HTMLElement {
    adminMenu.classList.remove("hidden");
   }
 
+  // // DEV MODE
+  // if (DEV_SIDEBAR_OPEN) {
+  //  mobileMenu.classList.add("open");
+  //  hamburger.classList.add("active");
+  // }
+
   // hamburger
   hamburger.addEventListener("click", () => {
-  mobileMenu.classList.toggle("open");
-  hamburger.classList.toggle("active");
+   mobileMenu.classList.toggle("open");
+   hamburger.classList.toggle("active");
   });
 
   // logout
